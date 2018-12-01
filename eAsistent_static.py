c372b3369129c5373ed942dc749de03be1f0a5ad
AyyItsAljo <aljaz.medic@gmail.com>
2018-12-01 21:52:50 +0100

    eAsistent_static.py
    Finished. Requesting works!

--- eAsistent_static.py
+++ eAsistent_static.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import requests, time, datetime, pytz, json
 from datetime import timezone
 from bs4 import BeautifulSoup
@@ -21,90 +19,121 @@ def extractTimestamp(input_dt):
 	#timestamp[int]
 	return int((time.mktime(input_dt.timetuple()) + input_dt.microsecond/1000000.0)*1000)
 
+def extractDatetime(int_ts):
+	return datetime.datetime.fromtimestamp(int_ts / 1e3)
 
-def getData(specials=True, log=False, full=False, week=getCurrentSchoolWeek()):
-	form = {}
-	form["id_profesor"] = 0
-	form["id_ucilnica"] = 0
-	form["qversion"] = 1
+def getData(specials_include=True, save_to_file=[True, False], log=False, full=False, week=getCurrentSchoolWeek()):
+	ret_object = {
+		"week":week[0],
+		"year":week[1],
+		"timetable":"N/V",
+		"specials":"N/V",
+		"sent_form":"N/V",
+		"error":None
+	}
+	try:
+		form = {
+			"id_profesor": 0,
+			"id_ucilnica" :0,
+			"qversion":1,
 
-	#week = (teden, leto)
-	form["teden"] = week[0]
-	form["id_sola"] = "632"
-	form["id_razred"] = "304912"
-	form["id_dijak"] = "8823701"
-	url = "https://www.easistent.com/urniki/ajax_urnik"
-	s = requests.Session()
+			#week = (teden, leto)
+			"teden" : week[0],
+			"id_sola" : "632",
+			"id_razred" : "304912",
+			"id_dijak" : "8823701"
+		}
+		url = "https://www.easistent.com/urniki/ajax_urnik"
+		s = requests.Session()
 
-	post_data = s.post(url, data=form)
-	post_data.encoding = 'ISO-8859-1'
+		post_data = s.post(url, data=form)
+		post_data.encoding = 'ISO-8859-1'
+
+		if save_to_file[0]:
+			with open("temp/last_eAsistent_static.html", "wb") as wf:
+				wf.write(post_data.content)
+
+
+		#handling the table
+		dnevi = []
+		urnik = {}
+		posebnosti = []
+		soup = BeautifulSoup(post_data.content, 'html.parser')
+
+		main_table = soup.find("table", class_="ednevnik-seznam_ur_teden")
+
+		for day_col in main_table.find("tr").findChildren("th")[1:]: #ignore "Ura"
+			#print(day_col.find_all("div")[1].getText() + " %i"%week[1])
+			day_date = datetime.datetime.strptime(day_col.find_all("div")[1].getText() + " %i"%week[1], '%d. %m. %Y')#.astimezone(pytz.utc)
+			day_name = day_col.find_all("div")[0].getText()
+			#(datetime, String)
+			dnevi.append((day_date, day_name))
+			
+		for row_index, current_row in enumerate(main_table.findChildren("tr", recursive=False)[1:]):
+			urnik[1] = []
+			cells = current_row.findChildren("td", recursive=False)
+			time_offset = abs(datetime.datetime.strptime(cells[0].find_all("div")[1].getText().split(" - ")[0], '%H:%M') - datetime.datetime(1900, 1, 1, 0, 0, 0, 0))
+			#(time of the day[time_delta], name[String])
+			day_time_tuple = (time_offset, cells[0].find_all("div")[0].getText())
+			cells = cells[1:]
+			for col_num, current_subject in enumerate(cells):
+				subject_data = {
+					"subject_abbr":"N/A",
+					"subject":"N/A",
+					"time":0,
+					"time_print":"",
+					"change":None,
+					"prof":"N/A",
+					"place":"N/A"
+				}
+
+				#set time
+				#(datetime of subject[datetime], name[String])
+				tuple_of_subject_time = (dnevi[col_num][0] + day_time_tuple[0], dnevi[col_num][1] + ", " +day_time_tuple[1])
+				#print(tuple_of_subject_time)
+
+				subject_data["time"] = extractTimestamp(tuple_of_subject_time[0])
+				subject_data["time_print"] = tuple_of_subject_time[1]
+
+				if(current_subject.getText()=="\n"):
+					#Lunch time
+					continue
+
+				#otherwise get subject info
+				subject_parse, prof_place = current_subject.find("div").findChildren(True , recursive=False)
+
+				grpL_name_spec = subject_parse.find("tr").findChildren("td", recursive=False)
+				subject_name, specialty = grpL_name_spec[:2]
+
+				subject_data["subject"] = subject_name.find("span")["title"]
+				subject_data["subject_abbr"] = subject_name.getText().strip()
+				subject_data["prof"], subject_data["place"] = prof_place.getText().strip().split(", ")
+
+				#check specialty
+				specialty_img = specialty.find("img")
+				if(specialty_img):
+					subject_data["change"] = specialty_img["title"]
+					if specials_include:
+						posebnosti.append((extractTimestamp(tuple_of_subject_time[0]), tuple_of_subject_time[1]))
+
+				#print(json.dumps(subject_data, indent=4))
+				urnik[extractTimestamp(tuple_of_subject_time[0])] = subject_data.copy()
+
+	except Exception as e:
+		ret_object["error"] = str(e)
+	else:
+		ret_object["timetable"] = urnik
+		ret_object["specials"] = posebnosti
+		ret_object["sent_form"] = form
+		if save_to_file[1]:
+			with open("temp/last_eAsistent_static.json", "w") as wf:
+				json.dump(ret_object, wf, indent=4)
 
 	if log:
-		with open("temp/last_eAsistent_static.html", "wb") as wf:
-			wf.write(post_data.content)
+		print(json.dumps(ret_object, indent=4))
+	return ret_object
 
 
-
-
-	#handling the table
-	dnevi = []
-	urnik = {}
-	posebnosti = {}
-	soup = BeautifulSoup(post_data.content, 'html.parser')
-
-	main_table = soup.find("table", class_="ednevnik-seznam_ur_teden")
-
-	for day_col in main_table.find("tr").findChildren("th")[1:]: #ignore "Ura"
-		#print(day_col.find_all("div")[1].getText() + " %i"%week[1])
-		day_date = datetime.datetime.strptime(day_col.find_all("div")[1].getText() + " %i"%week[1], '%d. %m. %Y')#.astimezone(pytz.utc)
-		day_name = day_col.find_all("div")[0].getText()
-		#(datetime, String)
-		dnevi.append((day_date, day_name))
-		
-	print (dnevi)
-	for row_index, current_row in enumerate(main_table.findChildren("tr", recursive=False)[1:]):
-		urnik[1] = []
-		cells = current_row.findChildren("td", recursive=False)
-		time_offset = abs(datetime.datetime.strptime(cells[0].find_all("div")[1].getText().split(" - ")[0], '%H:%M') - datetime.datetime(1900, 1, 1, 0, 0, 0, 0))
-		#(time of the day[time_delta], name[String])
-		day_time_tuple = (time_offset, cells[0].find_all("div")[0].getText())
-		cells = cells[1:]
-		for col_num, current_subject in enumerate(cells):
-			subject_data = {
-				"subject_abbr":"N/A",
-				"subject":"N/A",
-				"time":0,
-				"time_print":"",
-				"change":None,
-				"prof":"N/A",
-				"place":"N/A"
-			}
-
-			#(datetime of subject[datetime], name[String])
-			tuple_of_subject_time = (dnevi[col_num][0] + day_time_tuple[0], dnevi[col_num][1] + ", " +day_time_tuple[1])
-			#print(tuple_of_subject_time)
-
-			subject_data["time"] = extractTimestamp(tuple_of_subject_time[0])
-			subject_data["time_print"] = tuple_of_subject_time[1]
-
-			if(current_subject.getText()=="\n"):
-				#Lunch time
-				continue
-			subject_parse, prof_place = current_subject.find("div").findChildren(True , recursive=False)
-
-			grpL_name_spec = subject_parse.find("tr").findChildren("td", recursive=False)
-			subject_name, specialty, = grpL_name_spec[:2]
-
-			print(specialty)
-
-
-			subject_data["subject"] = subject_name.find("span")["title"]
-			subject_data["subject_abbr"] = subject_name.getText()
-			subject_data["prof"], subject_data["place"] = prof_place.getText().strip().split(", ")
-			print(json.dumps(subject_data, indent=4))
-			#urnik[tuple_of_subject_time[0]]
-			break
-		print()
 
 if __name__ == "__main__":
-	getData(log=True, week=(12, 2018))
+	getData(save_to_file=[True, True], log=True)
