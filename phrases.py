0097ad6d61d02e5fc56efd697b34da7c8f11190d
AyyItsAljo <aljaz.medic@gmail.com>
2018-12-02 14:23:31 +0100

    Moved phrases from json to py

---
+++ phrases.py
@@ -0,0 +1,30 @@
+
+english_day_names = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+
+interactions = {
+    "greet_q": [
+        "What can I do for you?", 
+        "How can I help?", 
+        "How can I help you?",
+        "What do you want me to do?"
+    ], 
+    "greet": [
+        "Hi!", 
+        "Hello!",
+        "Whats up?"
+    ],
+	"end": [
+        "Bye!", 
+        "See ya soon!", 
+        "Goodbye!", 
+        "Catch you up later"
+    ]
+}
